166a73cfc59478cf3ee61bca609566f92af9e561
ppresnyakov <ppresnyakov@yandex.ru>
2020-02-12 22:25:31 +0300

    А тут мы создали программу, складывающую два числа.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+
+public class Main {
+        // А тут мы решили залить просто комменатрий.
+    public static void main(String[] args)
+
+    {
+        System.out.println("Привет как дела? Давай сложу числа?");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введи 1-ое -число: ");
+        int a = in.nextInt();
+        System.out.println("Введи 2-ое -число: ");
+        int b = in.nextInt();
+        System.out.print("Итоговая сумма: ");
+        System.out.print(a+b);
     }
 }
