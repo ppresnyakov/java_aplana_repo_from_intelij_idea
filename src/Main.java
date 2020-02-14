ff5bab18932f844cc522d4e1738a6b5fa6ca5b0f
ppresnyakov <ppresnyakov@yandex.ru>
2020-02-14 13:49:22 +0300

    Подправили основной Main.java Числа для складывания, теперь называются fistInt и secondInt, вместо a+b. Добавленые пробелы для читаемости кода.

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,16 @@
 import java.util.Scanner;
 
-
-
 public class Main {
-        // А тут мы решили залить просто комменатрий.
     public static void main(String[] args)
-
     {
         System.out.println("Привет как дела? Давай сложу числа?");
         Scanner in = new Scanner(System.in);
         System.out.println("Введи 1-ое -число: ");
-        int a = in.nextInt();
+        int firstInt = in.nextInt();
         System.out.println("Введи 2-ое -число: ");
-        int b = in.nextInt();
+        int secondInt = in.nextInt();
         System.out.print("Итоговая сумма: ");
-        System.out.println(a+b);
+        System.out.println(firstInt + secondInt);
         System.out.println(" Было приятно познакомиться.");
     }
 }
