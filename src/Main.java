1cf29ea4b724a56cfdc4200441bc6a3241d43dd8
ppresnyakov <ppresnyakov@yandex.ru>
2020-02-21 18:06:45 +0300

    Задание №9: Ввод/вывод данных для массива

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-        for (int i = 1; i < 51; i++) {
-            System.out.println(i);
+        int ArraySize;
+        System.out.println("Введите размер массива:");
+        Scanner in = new Scanner(System.in);
+        ArraySize = in.nextInt();
+
+        int MyArray[] = new int[ArraySize];
+
+        for (int i = 0; i<ArraySize; i++ ){
+            System.out.println("Введите "+ i + " элемент массива ");
+            MyArray[i] = in.nextInt();
         }
+
+        System.out.println("Элементы массива, умноженные на 2.");
+        for (int i = 0; i<ArraySize; i++ ){
+            System.out.print(MyArray[i]* 2 + " ");
+
+        }
+
     }
     }
 
