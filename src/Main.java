6eebe5982e749e58767bdabc90242647b764b6ea
ppresnyakov <ppresnyakov@yandex.ru>
2020-02-21 17:08:16 +0300

    Задача 4. Решение задачи про двоичное число

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Привет как дела? Давай сложу числа?");
+        int c,a,sum = 0;
+        System.out.println("Привет как дела? ВВедите число в бинарном формате. Я переведу его в десячтный вид.");
         Scanner in = new Scanner(System.in);
-        System.out.println("Введи 1-ое -число: ");
-        int firstInt = in.nextInt();
-        System.out.println("Введи 2-ое -число: ");
-        int secondInt = in.nextInt();
-        System.out.print("Итоговая сумма: ");
-        System.out.println(firstInt + secondInt);
-        System.out.println(" Было приятно познакомиться.");
+
+        System.out.println("Введи число в бинарном формате: ");
+        String str = in.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            c = str.charAt(str.length() - 1 - i);
+
+            if(c == '1' || c == '0') {
+                if (c == '1')
+                    a = 1;
+                else a = 0;
+            } else {
+                System.out.println("Двоичное число состоит только из 1 и 0. Неверный ввод.");
+                break;
+            }
+            sum += (a * Math.pow(2, i));
+        }
+        System.out.println(sum);
     }
 }
