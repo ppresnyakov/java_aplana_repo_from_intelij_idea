4315643d13d7b14f3a7da7cf02471eb9202983d6
Pavel <ppresnyakov@yandex.ru>
2020-08-14 16:53:34 +0300

    Изменил 4 задание по массиву.

--- src/Task_4_JAVA_2.java
+++ src/Task_4_JAVA_2.java
@@ -3,28 +3,39 @@ import java.util.Scanner;
 
 public class Task_4_JAVA_2 {
     public static void main(String[] args) {
-        int ArrayLenght = 19;
+        int ArrayLenght = 20;
         int MyArray[] = new int[ArrayLenght];
 
-        for (int i = 0; i < 10; i++) {
-            MyArray[i] = (int) (Math.random()*(-100));
-        }
-        for (int i = 9; i < 19; i++) {
-            MyArray[i] = (int) (Math.random()*(100));
-        }
+        for (int i = 0; i < 20; i++) {
+            MyArray[i] = (int) (Math.random()*20 - 10);}
+
         System.out.println("Получили массив:");
         for (int i = 0; i < ArrayLenght; i++) {
             System.out.print(MyArray[i]+ " ");
         }
 
         //находим максимальный из отризательных
-        int MinMaxNegative = MyArray[0] ;
-         for (int i = 0; i < ArrayLenght; i++) { if (MyArray[i] < 0 && MyArray[i] > MinMaxNegative  ){MinMaxNegative = MyArray[i];}}
+        int MinMaxNegative = -11 ;
+        int IndexOfMinMaxNegative  =0;
+         for (int i = 0; i < ArrayLenght; i++) { if (MyArray[i] < 0 && MyArray[i] > MinMaxNegative  ){MinMaxNegative = MyArray[i]; IndexOfMinMaxNegative = i; }}
 
          //находим минимальный из положительный
-        int MaxMinPositive = MyArray[10] ;
-         for (int i = 0; i < ArrayLenght; i++) { if (MyArray[i] > 0 && MyArray[i] < MaxMinPositive  ){MaxMinPositive = MyArray[i];} }
+        int MaxMinPositive = 11 ;
+        int IndexOfMaxMinPositive = 0 ;
+         for (int i = 0; i < ArrayLenght; i++) { if (MyArray[i] > 0 && MyArray[i] < MaxMinPositive  ){MaxMinPositive = MyArray[i]; IndexOfMaxMinPositive = i ; }}
 
-        System.out.println(System.lineSeparator() + "Максимальный отрицательный: " + MinMaxNegative);
-        System.out.println("Минимальный положительный: " + MaxMinPositive);
+        System.out.println(System.lineSeparator() + "Максимальный отрицательный: " + MinMaxNegative +  ". Его индекс - " + IndexOfMinMaxNegative)  ;
+        System.out.println("Минимальный положительный: " + MaxMinPositive+  ". Его индекс - " + IndexOfMaxMinPositive);
+
+        // меняем местами
+        int Temp;
+        Temp = MyArray[IndexOfMinMaxNegative];
+        MyArray[IndexOfMinMaxNegative] = MyArray[IndexOfMaxMinPositive];
+        MyArray[IndexOfMaxMinPositive] = Temp;
+
+        System.out.println("Получили массив после замены:");
+        for (int i = 0; i < ArrayLenght; i++) {
+            System.out.print(MyArray[i]+ " ");
+        }
+
 }}
